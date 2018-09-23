9719d79777f2b2d8dadd59f1480f1473dda5a658
Juha Lindstedt <juha@pakastin.fi>
2018-09-23 16:59:10 +0300

    Fix child.off

--- index.js
+++ index.js
@@ -85,8 +85,6 @@ class Server {
         }
         this._starting = false;
         this._connected = false;
-
-        this.child.off('close', onClose);
       };
 
       this.child.on('close', onClose);
